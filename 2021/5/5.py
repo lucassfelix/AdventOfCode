c722aa3cff1241635455de6e1567f0319a81555b
Lucas Félix <lucas.felix.gamedev@gmail.com>
2021-12-07 20:56:02 -0300

    refactor

--- 2021/5/5.py
+++ 2021/5/5.py
@@ -1,38 +1,3 @@
-def exagero(points):
-    solution = {}
-
-    for i in range(len(points)):
-        for j in range(i,len(points)):
-            p1,p2 = points[i]
-            x1,y1 = p1
-            x2,y2 = p2
-            if(x1 != x2 and y1 != y2):
-                continue
-            p3,p4 = points[j]
-            x3,y3 = p3
-            x4,y4 = p4
-            if(x3 != x4 and y3 != y4):
-                continue
-            D = (x1 - x2)*(y3-y4) - (y1-y2)*(x3-x4)
-            if(D == 0):
-                print(p1)
-                for x in range(x1,x2+1):
-                    for y in range(y1,y2+1):
-                        if (x,y) not in solution:
-                            solution[(x,y)] = 1
-                        else:
-                            solution[(x,y)] += 1
-            else:
-                px = ((x1*y2-y1*x2)*(x3-x4) - (x1-x2)*(x3*y4 - y3*x4))/D
-                py = ((x1*y2-y1*x2)*(y3-y4) - (y1-y2)*(x3*y4 - y3*x4))/D
-                if (px,py) not in solution:
-                    solution[(px,py)] = 1
-                else:
-                    solution[(px,py)] += 1
-
-    result = sum(value >= 2 for value in solution.values())
-    return result
-
 def secondStar(points):
     solution = {}
 
