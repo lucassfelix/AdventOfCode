fdf64d26a57aaf6775c32ef2f57a9d1763a60533
Lucas Félix <lucas.felix.gamedev@gmail.com>
2023-12-11 09:56:59 -0300

    Added Point to base.cpp

--- base.cpp
+++ base.cpp
@@ -12,11 +12,15 @@ using namespace std;
 #define DEBUG 1
 #define ull unsigned long long
 #define ll long long
+#define Point pair<int,int>
+
+
+#if DEBUG == 1
+#endif
+
 
 int main()
 {
 
-#if DEBUG == 1
-#endif
 
 }
